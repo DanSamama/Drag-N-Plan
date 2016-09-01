115dd36e7b81e07154f35f6b57a451e8b03b5265
hilly noy <hilly88@gmail.com>
2016-09-01 20:52:06 +0300

    basic block structure (hard coded array)

--- scripts/repository.js
+++ scripts/repository.js
@@ -1,3 +1,22 @@
-/**
- * Created by itc_user on 9/1/2016.
- */
+'use strict';
+(function () {
+    var myApp = angular.module("dragNdrop", [])
+        .controller("repoController", function ($scope) {
+            $scope.title = "ITC Bootcamp, summer 2016";
+            $scope.titleDate = "1/5-21/9/2016"
+        });
+    
+        myApp.controller("blockController", function ($scope) {
+            var blocks = [
+                {type: "HTML", miniTitle: "Basic HTML", length: "30 Min"},
+                {type: "Java", miniTitle: "Intro to Java", length: "30 Min"},
+                {type: "SQL", miniTitle: "Intro to SQLight", length: "30 Min"},
+                {type: "CSS", miniTitle: "Responsive design", length: "30 Min"},
+                {type: "Java", miniTitle: "Jave advanced", length: "30 Min"},
+                {type: "SQL", miniTitle: "Databases", length: "30 Min"},
+                {type: "Java", miniTitle: "Advanced Java", length: "30 Min"},
+            ];
+            $scope.blocks = blocks;
+        })
+})
+();
